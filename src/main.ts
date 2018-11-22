924d20d16a7b5d63ce927484124836983cc491e9
franciscop-invast <36914428+franciscop-invast@users.noreply.github.com>
2018-11-22 13:23:23 +0900

    Added more flexibility

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-export const isInternalLink = (link: string) => /^\/(?!\/)/.test(link);
+// External links regexp, they start by one of those: 'http://', 'https://', '//'
+const external = /^(https?:\/\/|\/\/)/i;
+
+export const isInternalLink = (link: string) => !external.test(link);
