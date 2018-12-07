dee97f47f4ff8e397b74ba3a2de8194bf289c1f1
Muhammad Muhajir <muhajirframe@gmail.com>
2018-12-07 09:11:50 +0700

    fix fail test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // External links regexp, they start by one of those: 'http://', 'https://', '//'
 const external = /^(https?:\/\/|\/\/)/i;
 
-export const isInternalLink = (link: string) => false
+export const isInternalLink = (link: string) => !external.test(link);
 
 export default isInternalLink
